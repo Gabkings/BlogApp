e936414864dc9ac5e71e1dcc6a5666f35d16aa0b
Gabriel Gitonga <gabworks51@gmail.com>
2021-08-09 02:25:05 +0300

    Adding configurations and setup for the project

---
+++ api/src/database.providers.ts
@@ -0,0 +1,13 @@
+import { createConnection } from 'typeorm';
+
+export const databaseProviders = [
+  {
+    provide: 'DATABASE_CONNECTION',
+    useFactory: async () => await createConnection({
+      type: 'postgres',
+      url: process.env.DATABASE_URL,
+      synchronize: true,
+      
+    }),
+  },
+];
